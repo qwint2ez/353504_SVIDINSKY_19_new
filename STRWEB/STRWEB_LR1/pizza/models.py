06df4ec8d9738d4b2a5562f532a8a65c23504a15
svidinskyalexandr@gmail.com <svidinskyalexandr@gmail.com>
2025-09-17 19:01:55 +0300

    zavoz

--- STRWEB/STRWEB_LR1/pizza/models.py
+++ STRWEB/STRWEB_LR1/pizza/models.py
@@ -270,7 +270,6 @@ class Banner(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='banners/')
     is_active = models.BooleanField(default=True)
-    link = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
